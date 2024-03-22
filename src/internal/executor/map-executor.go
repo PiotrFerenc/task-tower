1dd98eb383c0ec5b34f377cbe1c3ec1b2439b049
Piotr <ferenc.piotr@outlook.com>
2024-03-22 09:52:19 +0100

    Refactor CreateMapExecutor in map-executor.go

--- src/internal/executor/map-executor.go
+++ src/internal/executor/map-executor.go
@@ -5,6 +5,7 @@ import (
 	"github.com/PiotrFerenc/mash2/src/api/types"
 	"github.com/PiotrFerenc/mash2/src/cmd/worker/actions"
 	"github.com/PiotrFerenc/mash2/src/internal/queues"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 )
 
@@ -16,38 +17,25 @@ type executor struct {
 }
 
 func CreateMapExecutor(queue queues.MessageQueue) Executor {
-	go func() {
+	a := map[string]actions.Action{
+		"hallo": actions.CreateHalloAction(),
+	}
 
+	go func() {
 		stage, err := queue.WaitingForStage()
 		if err != nil {
+			log.Fatal(err)
 		}
 		var forever chan struct{}
 
 		go func() {
 			for d := range stage {
-				var message types.Message
-				err := json.Unmarshal(d.Body, &message)
-				if err != nil {
-					panic(err)
-				}
-				a := map[string]actions.Action{
-					"hallo": actions.CreateHalloAction(),
-				}
-				action, _ := a[message.CurrentStage.Action]
+				message, err := unmarshal(d)
+
+				action := a[message.CurrentStage.Action]
 
 				message, err = action.Execute(message)
-				if err != nil {
-					err = queue.AddStageAsFailed(message)
-					if err != nil {
-
-						panic(err)
-					}
-				}
-
-				err = queue.AddStageAsSuccess(message)
-				if err != nil {
-					panic(err)
-				}
+				addToQueue(err, queue, message)
 			}
 		}()
 
@@ -58,6 +46,28 @@ func CreateMapExecutor(queue queues.MessageQueue) Executor {
 	return &executor{
 		queue: queue,
 	}
+}
+
+func addToQueue(err error, queue queues.MessageQueue, message types.Message) {
+	if err != nil {
+		err = queue.AddStageAsFailed(message)
+		if err != nil {
+			panic(err)
+		}
+	}
+	err = queue.AddStageAsSuccess(message)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func unmarshal(d amqp.Delivery) (types.Message, error) {
+	var message types.Message
+	err := json.Unmarshal(d.Body, &message)
+	if err != nil {
+		panic(err)
+	}
+	return message, err
 }
 
 func (executor *executor) Execute(actionName string, parameters types.Message) error {
