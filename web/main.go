f82a3c3f09e7020d54a0ca9623b813bf52af1b8c
Piotr <ferenc.piotr@outlook.com>
2024-03-29 18:32:00 +0100

    Refactor web interface and improve action handling

--- web/main.go
+++ web/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/PiotrFerenc/mash2/cmd/worker/actions"
+	"github.com/PiotrFerenc/mash2/internal/configuration"
+	"github.com/PiotrFerenc/mash2/internal/executor"
 	"github.com/labstack/echo/v4"
 	"html/template"
 	"io"
@@ -9,7 +12,7 @@ import (
 
 func main() {
 	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
+		templates: template.Must(template.ParseGlob("web/public/views/*.html")),
 	}
 	e := echo.New()
 	e.Renderer = t
@@ -17,8 +20,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		data := map[string]interface{}{
 			"Title":   "Strona główna",
-			"Header":  "Witaj w Echo",
-			"Message": "To jest strona główna wygenerowana z szablonu.",
+			"actions": mapItems(executor.CreateActionMap(&configuration.Config{})),
 		}
 		return c.Render(http.StatusOK, "index.html", data)
 	})
@@ -32,4 +34,21 @@ type Template struct {
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func mapItems(actions map[string]actions.Action) map[string]ActionParameter {
+	result := make(map[string]ActionParameter)
+	for key, action := range actions {
+		result[key] = ActionParameter{
+			Inputs:  action.Inputs(),
+			Outputs: action.Outputs(),
+		}
+	}
+	return result
+
+}
+
+type ActionParameter struct {
+	Inputs  []actions.Property
+	Outputs []actions.Property
 }
