220ce18ecfccdae01643a7b998b3eb985f8f23f9
Piotr <ferenc.piotr@outlook.com>
2024-04-01 10:26:33 +0200

    Refactor Docker actions and add port mapping

--- cmd/worker/actions/docker/docker-run.go
+++ cmd/worker/actions/docker/docker-run.go
@@ -2,11 +2,13 @@ package docker
 
 import (
 	"context"
+	"errors"
 	"github.com/PiotrFerenc/mash2/cmd/worker/actions"
 	"github.com/PiotrFerenc/mash2/internal/types"
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"strings"
 )
 
 func CreateDockerRun() actions.Action {
@@ -16,41 +18,58 @@ func CreateDockerRun() actions.Action {
 type dockerContainer struct {
 }
 
+const (
+	imageName = "image"
+	ports     = "ports"
+)
+
 func (c *dockerContainer) Inputs() []actions.Property {
-	output := make([]actions.Property, 1)
-	output[0] = actions.Property{
-		Name: "image",
-		Type: "text",
+	return []actions.Property{
+		{
+			Name: imageName,
+			Type: "text",
+		},
+		{
+			Name: ports,
+			Type: "text",
+		},
 	}
-	return output
 }
 func (c *dockerContainer) Outputs() []actions.Property {
-	output := make([]actions.Property, 1)
-	output[0] = actions.Property{
-		Name: "docker-container-id",
-		Type: "text",
+	return []actions.Property{
+		{Name: "container-id",
+			Type: "text",
+		},
 	}
-	return output
 }
+
 func (c *dockerContainer) Execute(process types.Pipeline) (types.Pipeline, error) {
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		return process, err
 	}
-	imageName, err := process.GetString("image")
+	imageName, err := process.GetString(imageName)
+
 	if err != nil {
 		return process, err
 	}
-	out, err := cli.ImagePull(ctx, imageName, image.PullOptions{})
+	ports, err := process.GetString(ports)
 	if err != nil {
-		panic(err)
+		return process, err
 	}
-	defer out.Close()
+
+	portMap, portSet, err := mapPorts(ports)
+	if err != nil {
+		return process, err
+	}
 
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: imageName,
-	}, nil, nil, nil, "")
+		Image:        imageName,
+		ExposedPorts: portSet,
+	}, &container.HostConfig{
+		PortBindings: portMap,
+	}, nil, nil, "")
 	if err != nil {
 		return process, err
 	}
@@ -59,12 +78,30 @@ func (c *dockerContainer) Execute(process types.Pipeline) (types.Pipeline, error
 		return process, err
 	}
 
-	out, err = cli.ContainerLogs(ctx, resp.ID, container.LogsOptions{ShowStdout: true})
-	if err != nil {
-		return process, err
-	}
-	defer out.Close()
-
-	process.SetString("docker-container-id", resp.ID)
+	process.SetString("container-id", resp.ID)
 	return process, nil
+}
+
+func mapPorts(ports string) (nat.PortMap, nat.PortSet, error) {
+	portPairs := strings.Split(ports, ", ")
+	portMap := nat.PortMap{}
+	portSet := nat.PortSet{}
+	for _, portPair := range portPairs {
+		portPair = strings.TrimSpace(portPair)
+		if !strings.Contains(portPair, ":") {
+			return nil, nil, errors.New("Invalid ports")
+		}
+		portParts := strings.Split(portPair, ":")
+		containerPort := portParts[0]
+		hostPort := portParts[1]
+		portMap[nat.Port(containerPort)] = []nat.PortBinding{
+			{
+				HostIP:   "0.0.0.0",
+				HostPort: hostPort,
+			},
+		}
+		portSet[nat.Port(containerPort)] = struct{}{}
+	}
+
+	return portMap, portSet, nil
 }
