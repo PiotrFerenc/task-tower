1e548867cd3d47b2c54d45cc3f1f57b1255029f1
Piotr <ferenc.piotr@outlook.com>
2024-04-15 11:53:36 +0200

    Add a new test case in main_test.go
    A new test case has been added to "main_test.go". This test case includes a HTTP POST request with a JSON payload. Additionally, new imports for "bytes", "net/http" and "time" were included to facilitate the creation of the test case.

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,8 +1,11 @@
 package tests
 
 import (
+	"bytes"
 	"context"
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	tc "github.com/testcontainers/testcontainers-go/modules/compose"
@@ -20,5 +23,31 @@ func TestSomething(t *testing.T) {
 	t.Cleanup(cancel)
 
 	require.NoError(t, compose.Up(ctx, tc.Wait(true)), "compose.Up()")
+
+	jsonData := `
+	{
+    "Parameters": {
+        "moje_imie" : "Jan",
+        "czesc.text": "Piotr"
+    },
+    "Stages": [
+        {
+            "Sequence": 1,
+            "Name": "czesc",
+            "Action": "console"
+        }
+    ]
+}
+`
+
+	reqBody := bytes.NewBufferString(jsonData)
+	//tak wiem...
+	time.Sleep(5 * time.Second)
+	resp, err := http.Post("http://localhost:5000/execute", "application/json", reqBody)
+	if err != nil {
+		require.NoError(t, err)
+	}
+	defer resp.Body.Close()
+	require.Equal(t, http.StatusOK, resp.StatusCode)
 
 }
