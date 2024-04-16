1e43b89da4ee46c65b69dbd0c4856becef4ca614
Piotr <ferenc.piotr@outlook.com>
2024-04-16 21:33:28 +0200

    Add method comments to process-service
    The code updates include the addition of explanation comments above the main functional methods in the process-service file. These detailed descriptions provide clarity on the purpose and functionality of each of the methods, aiming to improve code readability and maintainability.

--- internal/services/process-service.go
+++ internal/services/process-service.go
@@ -15,26 +15,37 @@ type processService struct {
 	repository repositories.ProcessRepository
 }
 
+// CreateProcessService creates a new instance of ProcessService with the given repository.
 func CreateProcessService(repository repositories.ProcessRepository) ProcessService {
 	return &processService{
 		repository: repository,
 	}
 }
 
+// MarkAsStarted saves the pipeline in the repository.
 func (process *processService) MarkAsStarted(pipeline *types.Process) {
 	process.repository.Save(*pipeline)
 }
+
+// TaskFinished sets the status of the pipeline to "Processing" and its current step to "Done".
+// It then updates the status of the pipeline in the repository.
 func (process *processService) TaskFinished(pipeline *types.Process) {
 	pipeline.Status = types.Processing
 	pipeline.CurrentStep.Status = types.Done
 	process.repository.UpdateStatus(*pipeline)
 }
 
+// MarkAsDone sets the status of the pipeline and its current step to "Done".
+// It then updates the status of the pipeline in the repository.
 func (process *processService) MarkAsDone(pipeline *types.Process) {
 	pipeline.Status = types.Done
 	pipeline.CurrentStep.Status = types.Done
 	process.repository.UpdateStatus(*pipeline)
 }
+
+// MarkAsFailed sets the status of the pipeline and its current step to "Fail",
+// and updates the error message. It then updates the status of the pipeline in the repository.
+// func (process *processService) MarkAsFailed(pipeline *types.Process, err string) {}
 func (process *processService) MarkAsFailed(pipeline *types.Process, err string) {
 	pipeline.Status = types.Fail
 	pipeline.Error = err
