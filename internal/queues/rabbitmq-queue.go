557af3b50adc79a7c073ad853808e28844801690
Piotr <ferenc.piotr@outlook.com>
2024-04-16 21:19:26 +0200

    Add detailed comments to rabbitmq-queue.go methods
    This commit adds comprehensive comments to each method in the rabbitmq-queue.go file. The comments provide a clear explanation about what each method does, its parameters, return values and any error scenarios, enhancing the overall readability of the code.

--- internal/queues/rabbitmq-queue.go
+++ internal/queues/rabbitmq-queue.go
@@ -21,6 +21,8 @@ type queue struct {
 	client        *RabbitClient
 }
 
+// CreateRabbitMqMessageQueue creates a new RabbitMQ message queue based on the given configuration.
+// It establishes a connection, creates the necessary queues, and returns the created queue.
 func CreateRabbitMqMessageQueue(configuration *configuration.QueueConfig) MessageQueue {
 	q := &queue{
 		configuration: configuration,
@@ -36,6 +38,10 @@ func CreateRabbitMqMessageQueue(configuration *configuration.QueueConfig) Messag
 	return q
 }
 
+// createQueues creates the necessary queues based on the provided configuration.
+// It iterates over the list of queue names in the configuration and calls the CreateQueue method
+// to create each queue using the RabbitMQ client.
+// Returns an error if any of the queue creations fail.
 func createQueues(q *queue, configuration *configuration.QueueConfig) error {
 	queues := []string{
 		configuration.QueueRunPipe,
@@ -51,10 +57,18 @@ func createQueues(q *queue, configuration *configuration.QueueConfig) error {
 	return nil
 }
 
+// CreateQueue creates a new queue with the specified name using the RabbitMQ client.
+// It uses the underlying RabbitClient's CreateQueue method to perform the creation.
+// Returns an error if the queue creation fails.
 func (q *queue) CreateQueue(name string) error {
 	return q.client.CreateQueue(name)
 }
 
+// Connect establishes a connection to the RabbitMQ server and initializes a RabbitClient.
+// It uses the underlying establishConnection function to establish the connection,
+// and then creates a new RabbitMQ client using the connection.
+// The resulting client is stored in the queue's client field for future use.
+// Returns an error if the connection or client creation fails.
 func (q *queue) Connect() error {
 	connection, err := q.establishConnection()
 	if err != nil {
@@ -71,6 +85,10 @@ func (q *queue) Connect() error {
 	return nil
 }
 
+// establishConnection establishes a connection to the RabbitMQ server using the provided
+// credentials and creates a RabbitClient with the connection.
+// It retries the connection every 1 second until a successful connection is established.
+// Returns the established connection and any error that occurred during the connection process.
 func (q *queue) establishConnection() (conn *amqp.Connection, err error) {
 	for {
 		conn, err = ConnectRabbitMQ(q.configuration.QueueUser, q.configuration.QueuePassword, q.configuration.QueueHost, q.configuration.QueuePort, q.configuration.QueueVhost)
@@ -85,19 +103,39 @@ func (q *queue) establishConnection() (conn *amqp.Connection, err error) {
 	return conn, nil
 }
 
+// WaitingForFailedTask returns a channel of amqp.Delivery that receives messages from the `QueueTaskFailed` queue.
+// It uses the `waitingForTask` method with the `QueueTaskFailed` queue name to retrieve the channel.
+// Returns the channel and any error that occurred during the process.
 func (q *queue) WaitingForFailedTask() (<-chan amqp.Delivery, error) {
 	return q.waitingForTask(q.configuration.QueueTaskFailed)
 }
+
+// WaitingForSucceedTask returns a channel of amqp.Delivery that receives messages from
+// the `QueueTaskSucceed` queue. It uses the `waitingForTask` method with the `QueueTaskSucceed`
+// queue name to retrieve the channel. Returns the channel and any error that occurred during the process.
 func (q *queue) WaitingForSucceedTask() (<-chan amqp.Delivery, error) {
 	return q.waitingForTask(q.configuration.QueueTaskSucceed)
 }
+
+// WaitingForTask returns a channel of amqp.Delivery that receives messages from the
+// `QueueRunPipe` queue. It uses the `waitingForTask` method with the `QueueRunPipe`
+// queue name to retrieve the channel. Returns the channel and any error
+// that occurred during the process.
 func (q *queue) WaitingForTask() (<-chan amqp.Delivery, error) {
 	return q.waitingForTask(q.configuration.QueueRunPipe)
 }
+
+// WaitingForFinishedTask returns a channel of amqp.Delivery that receives messages from the `QueueTaskFinished` queue.
+// It uses the `waitingForTask` method with the `QueueTaskFinished` queue name to retrieve the channel.
+// Returns the channel and any error that occurred during the process.
 func (q *queue) WaitingForFinishedTask() (<-chan amqp.Delivery, error) {
 	return q.waitingForTask(q.configuration.QueueTaskFinished)
 }
 
+// `waitingForTask` returns a channel of amqp.Delivery that receives messages from the specified queue.
+// It uses the RabbitMQ client's `Consume` method to retrieve the channel.
+// The function sets the arguments for `Consume` and passes them to the client.
+// Returns the channel and any error that occurred during the process.
 func (q *queue) waitingForTask(queueName string) (<-chan amqp.Delivery, error) {
 	return q.client.ch.Consume(
 		queueName,
@@ -109,6 +147,10 @@ func (q *queue) waitingForTask(queueName string) (<-chan amqp.Delivery, error) {
 		nil,
 	)
 }
+
+// addTask publishes a message to the specified queue using the RabbitMQ client.
+// It marshals the provided message to JSON format and sets the message properties.
+// Returns an error if the task addition fails.
 func (q *queue) addTask(queueName string, message types.Process) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -129,28 +171,52 @@ func (q *queue) addTask(queueName string, message types.Process) error {
 		})
 }
 
+// AddTaskToQueue adds a task to the specified queue using the RabbitMQ client.
+// It calls the addTask method with the configuration's QueueRunPipe queue name and the provided message.
+// Returns an error if the task addition fails.
 func (q *queue) AddTaskToQueue(message types.Process) error {
 	return q.addTask(q.configuration.QueueRunPipe, message)
 }
+
+// AddTaskAsSuccess adds a task to the specified queue as a successful task using the RabbitMQ client.
+// It calls the addTask method with the configuration's QueueTaskSucceed queue name and the provided message.
+// Returns an error if the task addition fails.
 func (q *queue) AddTaskAsSuccess(message types.Process) error {
 	return q.addTask(q.configuration.QueueTaskSucceed, message)
 }
+
+// AddTaskAsFinished adds a task to the specified queue as a finished task using the RabbitMQ client.
+// It calls the addTask method with the configuration's QueueTaskFinished queue name and the provided message.
+// Returns an error if the task addition fails.
 func (q *queue) AddTaskAsFinished(message types.Process) error {
 	return q.addTask(q.configuration.QueueTaskFinished, message)
 }
+
+// AddTaskAsFailed addTask adds a task to the specified queue using the RabbitMQ client.
+// It marshals the message to JSON format and publishes it to the queue.
+// Returns an error if the task addition fails.
 func (q *queue) AddTaskAsFailed(error error, message types.Process) error {
 	return q.addTask(q.configuration.QueueTaskFailed, message)
 }
 
+// RabbitClient represents a client for interacting with RabbitMQ.
+//
+// conn: Represents RabbitMQ connection.
+// ch: Represents RabbitMQ channel.
 type RabbitClient struct {
 	conn *amqp.Connection
 	ch   *amqp.Channel
 }
 
+// ConnectRabbitMQ establishes a connection to the RabbitMQ server using the provided
+// credentials and returns the connection and any error that occurred during the connection process.
 func ConnectRabbitMQ(user, password, host, port, vhost string) (*amqp.Connection, error) {
 	return amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/%s", user, password, host, port, vhost))
 }
 
+// NewRabbitMQClient creates a new RabbitMQ client based on the provided connection.
+// It establishes a channel using the connection and returns the created RabbitClient.
+// Returns the created RabbitClient and any error that occurred during the process.
 func NewRabbitMQClient(connection *amqp.Connection) (RabbitClient, error) {
 	ch, err := connection.Channel()
 	if err != nil {
@@ -162,6 +228,9 @@ func NewRabbitMQClient(connection *amqp.Connection) (RabbitClient, error) {
 	}, nil
 }
 
+// CreateQueue creates a new queue with the specified name using the RabbitMQ client.
+// It uses the underlying RabbitClient's QueueDeclare method to perform the creation.
+// Returns an error if the queue creation fails.
 func (rc RabbitClient) CreateQueue(name string) error {
 	_, err := rc.ch.QueueDeclare(
 		name,
@@ -174,6 +243,9 @@ func (rc RabbitClient) CreateQueue(name string) error {
 	return err
 }
 
+// Close closes the RabbitMQ channel associated with the RabbitClient instance.
+// It uses the underlying RabbitMQ channel's Close() method to close the channel.
+// Returns an error if there is an issue closing the channel.
 func (rc RabbitClient) Close() error {
 	return rc.ch.Close()
 }
