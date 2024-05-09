573530ea7d17524cefdf89cb4deade78475765ca
Piotr <ferenc.piotr@outlook.com>
2024-05-09 11:53:36 +0200

    Refactor install.sh to use variable names
    The commit refactors the install.sh script to use variables for container names. Specifically, two variables, 'controller_container' and 'worker_container', were introduced to replace the static names. This makes the script more readable and easy to maintain.

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-docker ps -q --filter "ancestor=task-tower/controller" | grep -q . && docker stop $(docker ps -q --filter "ancestor=task-tower/controller") || echo "No container to stop"
-docker ps -q --filter "ancestor=task-tower/worker" | grep -q . && docker stop $(docker ps -q --filter "ancestor=task-tower/worker") || echo "No container to stop"
-docker ps -aq --filter "ancestor=task-tower/controller" | grep -q . && docker rm -f $(docker ps -aq --filter "ancestor=task-tower/controller") || echo "No container to remove"
-docker ps -aq --filter "ancestor=task-tower/worker" | grep -q . && docker rm -f $(docker ps -aq --filter "ancestor=task-tower/worker") || echo "No container to remove"
+#!/usr/bin/zsh
+controller_container="task-tower/controller"
+worker_container="task-tower/worker"
+
+docker ps -q --filter "ancestor=$controller_container" | grep -q . && docker stop $(docker ps -q --filter "ancestor=$controller_container") || echo "No container to stop"
+docker ps -q --filter "ancestor=$worker_container" | grep -q . && docker stop $(docker ps -q --filter "ancestor=$controller_container") || echo "No container to stop"
+
+docker ps -aq --filter "ancestor=$worker_container" | grep -q . && docker rm -f $(docker ps -aq --filter "ancestor=$worker_container") || echo "No container to remove"
+docker ps -aq --filter "ancestor=$controller_container" | grep -q . && docker rm -f $(docker ps -aq --filter "ancestor=$worker_container") || echo "No container to remove"
